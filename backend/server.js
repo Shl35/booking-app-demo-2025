378ce234792c5e207588f8506c1201c77aa32183
Surachai Pimsalee <30917932+surachai-p@users.noreply.github.com>
2026-03-12 13:58:26 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -158,7 +158,7 @@ app.post('/api/login', (req, res) => {
     const token = jwt.sign(
       { id: user.id, username: user.username, role: user.role },
       JWT_SECRET,
-      { expiresIn: '5s' }
+      { expiresIn: '1h' }
     );
 
     res.json({
